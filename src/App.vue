00a0d2943a9f134c01201de022a91df42d7ce8fe
BedoyaAlban <a.bedoya@outlook.fr>
2020-09-30 19:17:33 +0200

    Preparing the component for fetching url

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NavBar />
+    <HackerNews />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
+import HackerNews from "./components/HackerNews.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    NavBar,
+    HackerNews
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/HackerNews.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Hacker News</h1>
+</template>
+
+<script>
+export default {
+  setup() {
+    async function getResults() {
+      const url = "http://hn.algolia.com/api/v1/search?query=front_page";
+      const response = await fetch(url, {
+        headers: {
+          accept: "application/json"
+        }
+      });
+      const data = await response.json();
+      console.log(data.hits);
+    }
+
+    getResults();
+  }
+};
+</script>
